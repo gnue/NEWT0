959e08020adef956f31dba3d7935bede6a0c8dbd
gnue <gnue@so-kukan.com>
2005-08-11 23:18:20 +0000

    added Getch global function

--- src/newt_core/NewtVM.c
+++ src/newt_core/NewtVM.c
@@ -3505,6 +3505,7 @@ void NVMInitExGlobalFns(void)
 
     NewtDefGlobalFunc(NSSYM(Gets),		NsGets,				0, "Gets()");
     NewtDefGlobalFunc(NSSYM(Getc),		NsGetc,				0, "Getc()");
+    NewtDefGlobalFunc(NSSYM(Getch),		NsGetch,			0, "Getch()");
 }
 
 
