d3fff41a5abb3ae197ca1f1745806d2d4fab56ca
Steve White <swhite@lekkerste.local>
2012-03-25 14:30:17 -0700

    "Register" the libraries we manually loaded, so any Requires don't fail

--- iNewt/iNewt/iNewt.c
+++ iNewt/iNewt/iNewt.c
@@ -12,11 +12,28 @@ extern void NativeCalls_install(void);
 extern void protoFILE_install(void);
 extern void protoREGEX_install(void);
 
+void iNewt_RegisterLibrary(char *library) {
+	newtRefVar requires = NcGetGlobalVar(NSSYM0(requires));
+  if (! NewtRefIsFrame(requires))
+	{
+		requires = NcMakeFrame();
+		NcDefGlobalVar(NSSYM0(requires), requires);
+	}
+
+  NcSetSlot(requires, NewtMakeSymbol(library), NewtMakeString(library, true));
+}
+
 void iNewt_Setup(void) {
   NewtInit(0, NULL, 0);
+
   protoFILE_install();
+  iNewt_RegisterLibrary("protoFILE");
+  
   protoREGEX_install();
+  iNewt_RegisterLibrary("protoREGEX");
+  
   NativeCalls_install();
+  iNewt_RegisterLibrary("NativeCalls");
 }
 
 void iNewt_Cleanup(void) {
