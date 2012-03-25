fc4912c96983c555306dc359bb52f89ded3cc964
Steve White <swhite@lekkerste.local>
2012-03-25 09:59:30 -0700

    Can't load libraries on iOS.

--- contrib/NewtObjC/NewtObjC.m
+++ contrib/NewtObjC/NewtObjC.m
@@ -60,71 +60,83 @@ void newt_install(void);
 
 void newt_install(void)
 {
+#if !TARGET_OS_IPHONE
 	// Load the objc-runtime extension dynamic library.
 	if (!NSAddLibraryWithSearching("@loader_path/objc-runtime-x.dylib")	// Tiger and higher.
 		&& !NSAddLibraryWithSearching("@executable_path/objc-runtime-x.dylib") // In-place.
 		&& !NSAddLibraryWithSearching(__LIBDIR__ "/objc-runtime-x.dylib")) // Installed path.
 	{
 		(void) NewtThrow(kNErrObjCRuntimeErr, NewtMakeString("Couldn't find objc-runtime-x.dylib", true));
-	} else {	
-		// Add the GetObjCClass global function.
-		NewtDefGlobalFunc(
-			NSSYM(GetObjCClass),
-			GetObjCClass,
-			1,
-			"GetObjCClass(name)");
-	
-		// Add the CreateObjCClass global function.
-		NewtDefGlobalFunc(
-			NSSYM(CreateObjCClass),
-			CreateObjCClass,
-			1,
-			"CreateObjCClass(def)");
-	
-		// Add the list of known classes (it's actually a frame).
-		NcSetGlobalVar(NSSYM(ObjCClasses), kNewtRefNIL);
-	
-		// Create kObjCTrue and kObjCFalse (chr(0) and chr(1)).
-		NcSetGlobalVar(NSSYM(kObjCTrue), NewtMakeCharacter(1));
-		NcSetGlobalVar(NSSYM(kObjCFalse), NewtMakeCharacter(0));
-		
-		// Create kObjCActionFuncType
-		NcSetGlobalVar(NSSYM(kObjCActionFuncType),
-			NewtMakeString(kObjCActionFuncTypeStr, true));
-	
-		// Create kObjCOutletType
-		NcSetGlobalVar(NSSYM(kObjCOutletType),
-			NewtMakeString(kObjCOutletTypeStr, true));
-			
-		// Create magic pointer (for instance variable methods)
-		newtRefVar magicPtr = NcMakeFrame();
-		
-		NcSetSlot(magicPtr,
-			NSSYM(InstanceVariableExists),
-			NewtMakeNativeFunc(
-				InstanceVariableExists, 1, "InstanceVariableExists(name)"));
-		NcSetSlot(magicPtr,
-			NSSYM(GetInstanceVariable),
-			NewtMakeNativeFunc(
-				GetInstanceVariable, 1, "GetInstanceVariable(name)"));
-		
-		NcDefMagicPointer(kInstanceParentMagicPtrKey, magicPtr);
-	
-		// Add the NSApplicationMain global function.
-		NewtDefGlobalFunc(
-			NSSYM(NSApplicationMain),
-			NewtNSApplicationMain,
-			1,
-			"NSApplicationMain(argv)");
-	
-		// Add the NSLog global function.
-		NewtDefGlobalFunc0(
-			NSSYM(NSLog),
-			NewtNSLog,
-			1,
-			true,
-			"NSLog(format, ...)");
+    return;
 	}
+#endif
+  
+  // Add the GetObjCClass global function.
+  NewtDefGlobalFunc(
+                    NSSYM(GetObjCClass),
+                    GetObjCClass,
+                    1,
+                    "GetObjCClass(name)");
+	
+  // Add the CreateObjCClass global function.
+  NewtDefGlobalFunc(
+                    NSSYM(CreateObjCClass),
+                    CreateObjCClass,
+                    1,
+                    "CreateObjCClass(def)");
+	
+  // Add the list of known classes (it's actually a frame).
+  NcSetGlobalVar(NSSYM(ObjCClasses), kNewtRefNIL);
+	
+  // Create kObjCTrue and kObjCFalse (chr(0) and chr(1)).
+  NcSetGlobalVar(NSSYM(kObjCTrue), NewtMakeCharacter(1));
+  NcSetGlobalVar(NSSYM(kObjCFalse), NewtMakeCharacter(0));
+  
+  // Create kObjCActionFuncType
+  NcSetGlobalVar(NSSYM(kObjCActionFuncType),
+                 NewtMakeString(kObjCActionFuncTypeStr, true));
+	
+  // Create kObjCOutletType
+  NcSetGlobalVar(NSSYM(kObjCOutletType),
+                 NewtMakeString(kObjCOutletTypeStr, true));
+  
+  // Create magic pointer (for instance variable methods)
+  newtRefVar magicPtr = NcMakeFrame();
+  
+  NcSetSlot(magicPtr,
+            NSSYM(InstanceVariableExists),
+            NewtMakeNativeFunc(
+                               InstanceVariableExists, 1, "InstanceVariableExists(name)"));
+  NcSetSlot(magicPtr,
+            NSSYM(GetInstanceVariable),
+            NewtMakeNativeFunc(
+                               GetInstanceVariable, 1, "GetInstanceVariable(name)"));
+  
+  NcDefMagicPointer(kInstanceParentMagicPtrKey, magicPtr);
+	
+#if !TARGET_OS_IPHONE
+  // Add the NSApplicationMain global function.
+  NewtDefGlobalFunc(
+                    NSSYM(NSApplicationMain),
+                    NewtNSApplicationMain,
+                    1,
+                    "NSApplicationMain(argv)");
+#else
+  // Add the UIApplicationMain global function.
+  NewtDefGlobalFunc(
+                    NSSYM(UIApplicationMain),
+                    NewtUIApplicationMain,
+                    1,
+                    "UIApplicationMain(argv)");
+#endif
+  
+  // Add the NSLog global function.
+  NewtDefGlobalFunc0(
+                     NSSYM(NSLog),
+                     NewtNSLog,
+                     1,
+                     true,
+                     "NSLog(format, ...)");
 }
 
 // =================================================== //
